06d456aed5753272ff605a4fe8089bd0490a5752
Blowerlop <chrisazur@gmx.fr>
2022-09-11 14:10:43 +0200

    Refactoring FirstPersonController

--- Sandbox - Features/Assets/Features/ThirdPersonController/InputManager.cs
+++ Sandbox - Features/Assets/Features/ThirdPersonController/InputManager.cs
@@ -5,7 +5,27 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    public void Fire(InputAction.CallbackContext context)
+    // Variables
+    public Vector2 move;
+    public Vector2 look;
+    public bool isSprinting = false;
+    public bool isJumping = false;
+
+
+
+    // Methods
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        look = context.ReadValue<Vector2>();
+        
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
     {
         if (context.performed) { Debug.Log("Fire"); }
     }
--- Sandbox - Features/Assets/Features/ThirdPersonController/PlayerController.cs
+++ Sandbox - Features/Assets/Features/ThirdPersonController/PlayerController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	/*
 	[Header("Player")]
 	[Tooltip("Move speed of the character in m/s")]
 	public float MoveSpeed = 4.0f;
@@ -59,26 +58,13 @@ public class PlayerController : MonoBehaviour
 	private float _fallTimeoutDelta;
 
 
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
-	private PlayerInput _playerInput;
-#endif
+	private InputManager _inputManager;
+
 	private CharacterController _controller;
-	private StarterAssetsInputs _input;
 	private GameObject _mainCamera;
 
 	private const float _threshold = 0.01f;
 
-	private bool IsCurrentDeviceMouse
-	{
-		get
-		{
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
-			return _playerInput.currentControlScheme == "KeyboardMouse";
-#else
-				return false;
-#endif
-		}
-	}
 
 	private void Awake()
 	{
@@ -92,12 +78,9 @@ public class PlayerController : MonoBehaviour
 	private void Start()
 	{
 		_controller = GetComponent<CharacterController>();
-		_input = GetComponent<StarterAssetsInputs>();
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
-		_playerInput = GetComponent<PlayerInput>();
-#else
-			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
-#endif
+		_inputManager = GetComponent<InputManager>();
+
+
 
 		// reset our timeouts on start
 		_jumpTimeoutDelta = JumpTimeout;
@@ -126,13 +109,13 @@ public class PlayerController : MonoBehaviour
 	private void CameraRotation()
 	{
 		// if there is an input
-		if (_input.look.sqrMagnitude >= _threshold)
+		if (_inputManager.look.sqrMagnitude >= _threshold)
 		{
 			//Don't multiply mouse input by Time.deltaTime
-			float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
+			float deltaTimeMultiplier = 1.0f;
 
-			_cinemachineTargetPitch += _input.look.y * RotationSpeed * deltaTimeMultiplier;
-			_rotationVelocity = _input.look.x * RotationSpeed * deltaTimeMultiplier;
+			_cinemachineTargetPitch += _inputManager.look.y * RotationSpeed * deltaTimeMultiplier;
+			_rotationVelocity = _inputManager.look.x * RotationSpeed * deltaTimeMultiplier;
 
 			// clamp our pitch rotation
 			_cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
@@ -148,19 +131,19 @@ public class PlayerController : MonoBehaviour
 	private void Move()
 	{
 		// set target speed based on move speed, sprint speed and if sprint is pressed
-		float targetSpeed = _input.sprint ? SprintSpeed : MoveSpeed;
+		float targetSpeed = _inputManager.isSprinting ? SprintSpeed : MoveSpeed;
 
 		// a simplistic acceleration and deceleration designed to be easy to remove, replace, or iterate upon
 
 		// note: Vector2's == operator uses approximation so is not floating point error prone, and is cheaper than magnitude
 		// if there is no input, set the target speed to 0
-		if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+		if (_inputManager.move == Vector2.zero) targetSpeed = 0.0f;
 
 		// a reference to the players current horizontal velocity
 		float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
 
 		float speedOffset = 0.1f;
-		float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
+		float inputMagnitude = 1f;
 
 		// accelerate or decelerate to target speed
 		if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
@@ -178,14 +161,14 @@ public class PlayerController : MonoBehaviour
 		}
 
 		// normalise input direction
-		Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
+		Vector3 inputDirection = new Vector3(_inputManager.move.x, 0.0f, _inputManager.move.y).normalized;
 
 		// note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
 		// if there is a move input rotate player when the player is moving
-		if (_input.move != Vector2.zero)
+		if (_inputManager.move != Vector2.zero)
 		{
 			// move
-			inputDirection = transform.right * _input.move.x + transform.forward * _input.move.y;
+			inputDirection = transform.right * _inputManager.move.x + transform.forward * _inputManager.move.y;
 		}
 
 		// move the player
@@ -206,7 +189,7 @@ public class PlayerController : MonoBehaviour
 			}
 
 			// Jump
-			if (_input.jump && _jumpTimeoutDelta <= 0.0f)
+			if (_inputManager.isJumping && _jumpTimeoutDelta <= 0.0f)
 			{
 				// the square root of H * -2 * G = how much velocity needed to reach desired height
 				_verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
@@ -230,7 +213,7 @@ public class PlayerController : MonoBehaviour
 			}
 
 			// if we are not grounded, do not jump
-			_input.jump = false;
+			_inputManager.isJumping = false;
 		}
 
 		// apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
@@ -258,6 +241,6 @@ public class PlayerController : MonoBehaviour
 		// when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
 		Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z), GroundedRadius);
 	}
-	*/
+	
 }
 
